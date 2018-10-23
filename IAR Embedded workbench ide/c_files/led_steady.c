bf1df9bda7c724ce93bdf9002303a06013ea10e6
Wan Azmanuddin <azman.ir93@gmail.com>
2018-10-23 14:33:36 +0200

    Update 23 oct 2018

--- IAR Embedded workbench ide/c_files/led_steady.c
+++
@@ -1,8 +0,0 @@
-#include "io430.h"
-int main( void )
-{
-  WDTCTL = WDTPW + WDTHOLD;
-  P1DIR |= 0x03;
-  P1OUT |= 0x03;
-  while(1);
-}
--- IAR Embedded workbench ide/drivers/gradient/sensing.h
+++
@@ -1,8 +0,0 @@
-#ifndef SENSING_H
-#define SENSING_H
-
-int     get_msp_temperature();
-int     get_adc_temperatures(uint8_t input);
-uint8_t get_battery();
-
-#endif
